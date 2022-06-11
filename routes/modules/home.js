24098f31582ab79dc326240aa227725c86350368
Yen <p12427@gmail.com>
2022-06-12 02:39:40 +0800

    feat: set 'post original URL' router

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const Url = require('../../models/url')
+const generateRandomCode = require('../../utils/generate_random_code')
 
 // set router: get homepage
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+// set router: post original URL
+router.post('/', (req, res) => {
+  const { originalUrl } = req.body
+  const randomCode = generateRandomCode(5)
+  const shortenedUrl = `http://${req.headers.host}/${randomCode}`
+  res.render('index', { shortenedUrl, originalUrl })
 })
 
 module.exports = router
